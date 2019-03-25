dbfa4dac2c64ec8ae948657350cce57964e532e2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-25 12:58:03 +0000

    Deploy tomastrejdl/esp8266-home-automation to github.com/tomastrejdl/esp8266-home-automation.git:gh-pages

---
+++ precache-manifest.c097ba6389858092422e930d27c0c77d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "75027e6bdffb6124706d",
+    "url": "/esp8266-home-automation/static/css/main.89c14a60.chunk.css"
+  },
+  {
+    "revision": "75027e6bdffb6124706d",
+    "url": "/esp8266-home-automation/static/js/main.75027e6b.chunk.js"
+  },
+  {
+    "revision": "ea76618ade1b96338d8c",
+    "url": "/esp8266-home-automation/static/js/runtime~main.ea76618a.js"
+  },
+  {
+    "revision": "b2f5ab1217bac76f1d55",
+    "url": "/esp8266-home-automation/static/js/2.b2f5ab12.chunk.js"
+  },
+  {
+    "revision": "b77f96d2224a8895c208963c1345e371",
+    "url": "/esp8266-home-automation/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/esp8266-home-automation/precache-manifest.c097ba6389858092422e930d27c0c77d.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/esp8266-home-automation/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
